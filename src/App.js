b846ade2a1d74df348d0f8069d82deced44d35d0
Nacho-sf <serranof.ignacio@gmail.com>
2022-09-21 22:14:28 +0200

    API_KEY

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Main from './components/Main';
 import Footer from './components/Footer';
 import { BrowserRouter } from 'react-router-dom';
 
-console.log (process.env.REACT_APP_API_KEY)
-
 function App() {
   return (
     <div className="App">
