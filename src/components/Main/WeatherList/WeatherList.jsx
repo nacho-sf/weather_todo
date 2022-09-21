588648d8fffcd7f3fb38ddf031d8233674324529
Nacho-sf <serranof.ignacio@gmail.com>
2022-09-21 17:37:02 +0200

    weather_done

--- src/components/Main/WeatherList/WeatherList.jsx
+++ src/components/Main/WeatherList/WeatherList.jsx
@@ -1,40 +1,72 @@
 import React, { Component } from 'react';
 import { v4 as uuidv4 } from "uuid"
+
 import WeatherCard from './WeatherCard'
 
 class WeatherList extends Component {
+
     constructor(props) {
         super(props);
 
-        this.state = { weatherList: this.props.defaultList }
         
+
+        this.state = { 
+            weatherList:this.props.defaultList
+        };
         console.log('CONSTRUCTOR')
-    }
+    };
     
+    
+
+
+
     async componentDidMount(){
-        
-        const resp = await fetch('https://api.openweathermap.org/data/2.5/forecast?lat=40.4167047&lon=-3.7035825&appid=5b7c3c118156d3cf093cbde48c30eb8b');
-        const data = await resp.json();
-        this.setState({
-            weatherList: data.list
-        })
+        this.getDataWeather("Madrid");
         console.log('componentDidMount');
     }
 
+    async getDataWeather(city){
+        const resp = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=5b7c3c118156d3cf093cbde48c30eb8b`);
+        const data = await resp.json();
+        
+        this.setState({
+            weatherList:data.list
+        })
+    }
+
+    handleChange = (e) => {
+        e.preventDefault();
+        const city = e.target.city.value;
+        alert(city);
+        alert("hola");
+        this.setState({ city });
+        this.getDataWeather(city);
+    }
 
     render() {
         console.log('RENDER')
         return (
-            <div>
+            <section>
+                <h3>Ciudad: {this.state.city}</h3>
+                <form onSubmit={this.handleChange}>
+                    <input type="text" id="city" name="city" />
+                    <input type="submit"  value="Enviar"/>
+                </form>
+
+                
+
+                <div>
                 <h3>Lista del tiempo</h3>
-                {
-                  this.state.weatherList.map(weathercard => 
-                    <WeatherCard data={weathercard} key={uuidv4()}/>
-                  )
-                }
-            </div>        
-        );
+                    {this.state.weatherList.map(weathercard => 
+                    <WeatherCard data={weathercard} key={uuidv4()}/>)}
+                </div>
+            </section>   
+            
+            
+        )
+        
     }
+
 }
 
 WeatherList.defaultProps = {
